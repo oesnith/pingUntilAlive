a1f4c4c47d3c144a3a5378be3fe853557f7001dc
Karl Minnich <karl.minnich@oesnith.net>
2020-07-21 16:34:09 -0400

    Pull pushover userkey and apikey in config file
    Added the ability to store your Pushover userkey and apikey in a config file so you don't have to supply them on the command line.

--- pingUntilAlive.sh
+++ pingUntilAlive.sh
@@ -1,27 +1,44 @@
 #!/bin/bash
 
-################################################################
-# pingUntilAlive.sh                                            #
-# Run a continuous ping check or port check until it's alive,  #
-# then stop and print the time it became alive.                #
-#                                                              #
-# Flags:                                                       #
-# -h :: host (Required - FQDN or IP)                           #
-# -c :: count (number of pings to send)                        #
-# -t :: timeout (number of seconds to wait for ping return)    #
-# -p :: port (for TCP port check)                              #
-#                                                              #
-################################################################
+#######################################################################################
+# pingUntilAlive.sh                                                                   #
+# Run a continuous ping check or port check until it's alive,                         #
+# then stop and print the time it became alive.                                       #
+#                                                                                     #
+# Flags:                                                                              #
+# -h :: host (Required - FQDN or IP)                                                  #
+# -c :: count (number of pings to send)                                               #
+# -t :: timeout (number of seconds to wait for ping return)                           #
+# -p :: port (for TCP port check)                                                     #
+# -n :: notification (Enable Pushover Notification)                                   #
+#       Must include USERKEY and APIKEY from your Pushover account delimited by '::'  #
+#       -n will override -N                                                           #
+# -N :: notification (Enable Pushover Notification) from config file.                 #
+#       Uses the Pushover API information from the config file.                       #
+#       Defined using -f, or using pingUntilAlive.conf located in script directory    #
+# -f :: config file - Full path.                                                      #
+#       Default: pingUntilAlive.conf located in the directory with this script        #
+#                                                                                     #
+#######################################################################################
 
 programname=$0
 usage () {
    echo
-   echo "Usage: $programname -h FQDN [-c count] [-t timeout] [-p port] [-n USERKEY|APIKEY]"
+   echo "Usage: $programname -h FQDN [-c count] [-t timeout] [-p port] [-n USERKEY|APIKEY] [-N] [-f filename]"
    echo "  -h	host (FQDN or IP)"
    echo "  -c	count (number of pings to send)"
    echo "  -t	timeout (number of second to wait for ping return)"
    echo "  -p	port (for TCP port check)"
-   echo "  -n	notification (Enable Pushover Notification - Must include USERKEY and APIKEY from your Pushover account delimited by '::')"
+   echo "  -n	notification (Enable Pushover Notification)"
+   echo "          Must include USERKEY and APIKEY from your Pushover account"
+   echo "          delimited by '::'" 
+   echo "          -n will override -N"
+   echo "  -N   notification (Enable Pushover Notification) from config file."
+   echo "          Uses the Pushover API information from the config file."
+   echo "          Defined using -f, or using pingUntilAlive.conf located in"
+   echo "          script directory"
+   echo "  -f   config file - Full path.  Default: pingUntilAlive.conf located"
+   echo "          in the directory with this script"
    exit 1
 }
 
@@ -58,10 +75,10 @@ if [[ "$prereq_failed" == "TRUE" ]] ; then
    echo "Please install before continuing."
    usage
 fi
+#Runtime variables
+   configfile="pingUntilAlive.conf"
 
-
-
-while getopts ":p:c:h:t:n:" opt; do
+while getopts ":p:c:h:t:f:n:N" opt; do
    case $opt in
       p) # -p :: Port to test (will test via TCP)
          port=$OPTARG
@@ -93,6 +110,13 @@ while getopts ":p:c:h:t:n:" opt; do
 	    usage	
          fi
          ;;
+      f) # -f Config File
+         configfile=$OPTARG 
+         if [ ! -f "$configfile" ] ; then
+            echo "Config file not found: $configfile"
+            usage
+         fi
+         ;;
       n) # -n Pushover notification - USERKEY|APIKEY
          PO_info=$OPTARG
          if [[ "$PO_info" != *"::"* ]]; then
@@ -101,6 +125,10 @@ while getopts ":p:c:h:t:n:" opt; do
          fi
          PO_UserKey=`echo $PO_info | awk -F "::" '{print $1}'`
          PO_ApiKey=`echo $PO_info | awk -F "::" '{print $2}'`
+         ;;
+      N) # -N Pushover notification - Get USERKEY and APIKEY from config file.  Default: pingUntilAlive.conf located in the same directory
+         #Need to process this AFTER the rest of the arguments are processed so the config file gets set properly
+         followup_N="TRUE"
          ;;
       \?)
          echo "Invalid option: -$OPTARG" >&2
@@ -113,6 +141,20 @@ while getopts ":p:c:h:t:n:" opt; do
    esac
 done
 
+#Do the followup processing of the -N flag.
+if [ "$followup_N" == "TRUE" ] ; then
+   if [ "$PO_UserKey" == "" ] || [ "$PO_ApiKey" == "" ] ; then
+      PO_UserKey=`grep PUSHOVER-USERKEY $configfile | awk '{print $2}'`
+      PO_ApiKey=`grep PUSHOVER-APIKEY $configfile | awk '{print $2}'`
+      PO_info="Filler"
+   fi
+   if [ "$PO_UserKey" == "" ] || [ "$PO_ApiKey" == "" ] ; then
+      echo "No PUSHOVER-USERKEY or PUSHOVER-API key values found in $configfile."
+      echo "Please add them to the file with a tab delimiter."
+      usage
+   fi
+
+fi
 # Test that the host flag is populated, everything else will default to something.
 if [ -z  "$host"  ]; then
    echo "Missing required -h flag. Please include the IP or FQDN."
